ae8e58bec478d2552cece3c818ad7e6c71547e01
dogustuluk <dogus.tuluk@gmail.com>
2022-08-09 15:30:01 +0300

    TaskConsoleApp.WhenAll - WaitAny

--- TaskConsoleApp.WhenAll/Program.cs
+++ TaskConsoleApp.WhenAll/Program.cs
@@ -25,6 +25,10 @@ namespace TaskConsoleApp.WhenAll
       //WaitAll(task1,task2,task3,...) metodu
       //task array alır ve task'ler tamamlanana kadar bekler. WhenAll metodundan farkı ise; whenAll metodu ana thread'i bloklamıyorken, WaitAll metodu bloklama işlemini gerçekleştirmektedir. UI Thread'i(Main thread'i) bloklamaktadır. Kullanılması çok fazla tercih edilmez. whenAll metodundan farkı ise parametre olarak milisaniye cinsinden bir parametre alır. Vermiş olduğumuz görevler parametrede vermiş olduğumuz süre içerisinde yapılırsa true, yapılmazsa false dönmektedir.
 
+      //WaitAny metodu
+      //WaitAll metodu gibi bloklayan bir yapıdadır. Nerede çağırılıyor ise o thread'i bloklar. array almaktadır. bu array içerisinde herhangi birisi tamamlandığı zaman geriye bir integer değer döner. Bu integer değer ise tamamlanan task'in index numarasıdır. bu index numarası üzerinden tamamlanan task'i alabiliriz. milisaniye alır.
+
+
         private async static Task Main(string[] args)
         {
             Console.WriteLine("Main Thread : " + Thread.CurrentThread.ManagedThreadId);
@@ -44,14 +48,24 @@ namespace TaskConsoleApp.WhenAll
                 taskList.Add(GetContentAsync(x));
             });
 
+            //WaitAny start
+            Console.WriteLine("WaitAny metodundan önce");
+            
+            var firstTaskIndex =Task.WaitAny(taskList.ToArray());
+
+            Console.WriteLine($"{taskList[firstTaskIndex].Result.Site} - {taskList[firstTaskIndex].Result.Len}");
+
+            Console.WriteLine("WaitAny metodundan sonra");
+            //WaitAny end
+
             //WaitAll start
-            Console.WriteLine("WaitAll metodundan önce");
-            //1.örnek// Task.WaitAll(taskList.ToArray());
-            //2.örnek
-            bool result = Task.WaitAll(taskList.ToArray(), 3000);
-            Console.WriteLine("3 saniyede geldi mi:" + result);
-            Console.WriteLine("WaitAll metodundan sonra");
-            Console.WriteLine($"{taskList.First().Result.Site} - {taskList.First().Result.Len}");
+            //Console.WriteLine("WaitAll metodundan önce");
+            ////1.örnek// Task.WaitAll(taskList.ToArray());
+            ////2.örnek
+            //bool result = Task.WaitAll(taskList.ToArray(), 3000);
+            //Console.WriteLine("3 saniyede geldi mi:" + result);
+            //Console.WriteLine("WaitAll metodundan sonra");
+            //Console.WriteLine($"{taskList.First().Result.Site} - {taskList.First().Result.Len}");
             //WaitAll end
 
 
