d7647e2413bc634d99fa671cadf5c902f265809b
dogustuluk <dogus.tuluk@gmail.com>
2022-09-13 12:38:36 +0300

    Task property: Result

---
+++ TaskInstance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskInstance
+{
+    /*
+     * Task Instance ->>>Propterty: Result
+     * async metot çağırdığımızda geriye yeni bir task instance'ı döner (nesne örneği).
+     * bu property ile dönen datayı alabiliriz. fakat bunun bir dezavantajı vardır. ->>> bu property'si o andaki thread'i bloklar.
+     * neden ihtiyaç duyulur ->> normal bir metot içerisinden async metot çağırmak istersek bunu kullanarak istediğimiz datayı alabiliriz.
+     */
+    internal class Program
+    {
+        private async static Task Main(string[] args)
+        {
+            Console.WriteLine(GetData());
+
+
+        }
+        public static string GetData()
+        {
+            var task = new HttpClient().GetStringAsync("https://www.google.com");
+            return task.Result; //task değişkeninin sonucu 5sn sürüyorsa bu satıra geldiğinde o anki thread'i bloklar. buradan sonuç dönünceye kadar thread bloklanacağından dolayı responsive'liği kaybederiz. form app ise pencere oynatması, herhangi bir butona tıklama gibi işlemleri yapamaz, ekran donacaktır.
+        }
+    }
+}
