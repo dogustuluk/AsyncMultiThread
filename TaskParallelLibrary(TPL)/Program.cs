1b67c78fdcbe649c6dd4186803b3a05dd2a90a32
dogustuluk <dogus.tuluk@gmail.com>
2022-09-13 14:18:14 +0300

    Parallel.ForEach part1

--- TaskParallelLibrary(TPL)/Program.cs
+++ TaskParallelLibrary(TPL)/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TaskParallelLibrary_TPL_
-{/*
+{
+    /*
   * task parallel library(tpl) nedir?
   * thirdparty değildir.
   * bu teknoloji ya da kütüphane ile kodlarımızı birden fazla thread'de zorlanmadan çalıştırabiliriz.
@@ -18,11 +25,55 @@ namespace TaskParallelLibrary_TPL_
      * paylaşımlı data kullanmadan yaparsak kodlarımız daha hızlı çalışacaktır çünkü herhangi bir lag mekanizması olmayacak.
      * örnek bir senaryo vermek gerekirse; a ve b thread'lerimiz ortak bir data üzerinde(int 5değerinde) veri artırımı yapsın. a thread'i t1 zamanda 5 değerini alsın ve b thread'i de t2 zamanında 5 değerini alsın. ardından a thread'i t3 zamanında 5 değerini 1 artırsın ve b thread'i de t4 zamanında 5 değerini 1 arttırsın. her iki thread'in de ulaşacağı nihai sonuç a için t5 ve b için t6 zamanında aynı olan 6 değeri olacaktır. fakat olması gereken sonuç b thread'inin 7 sonucunu döndürmesi olacaktır.
      */
+
+    /*
+     * Parallel.ForEach metodu
+     * içerisine almış olduğu bir array'i ve array içerisindeki itemleri farklı thread'lerde çalıştırarak multithread bir kod yazmamıza imkan verir.
+     * bunu nasıl yapar -> foreach array'i bu kütüphane belli parçalara ayırmaktadır. mesela 500 item'a sahip bir array verirsek, bunları 100 parçaya ayırıp her bir parçaya bir thread atar. ilgili thread kendisine alanan parçayı işlemeye çalışır.
+     * Eğer yapılacak olan işlem küçük ise performansı olumsuz etkilemektedir.
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            string picturesPath = @"C:\Users\dogus\Pictures\TPL";
+
+            var files = Directory.GetFiles(picturesPath);//dosyaları aktarmak için. her bir dosyanın yolunu alır.
+
+            Parallel.ForEach(files, (item) =>
+            {
+                Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+
+                Image img = new Bitmap(item);
+
+                var thumbnail = img.GetThumbnailImage(50, 50, () => false,IntPtr.Zero);
+
+                thumbnail.Save(Path.Combine(picturesPath, "thumbnail",Path.GetFileName(item)));
+
+
+            });
+            sw.Start();
+            Console.WriteLine("İşlem Bitti: "+sw.ElapsedMilliseconds);
+            
+            sw.Reset();
+            
+            sw.Start();
+            files.ToList().ForEach(x =>
+            {
+                Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+
+                Image img = new Bitmap(x);
+
+                var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+                thumbnail.Save(Path.Combine(picturesPath, "thumbnail", Path.GetFileName(x)));
+            });
+            sw.Stop(); Console.WriteLine("İşlem Bitti: " + sw.ElapsedMilliseconds);
+
+
         }
     }
 }
